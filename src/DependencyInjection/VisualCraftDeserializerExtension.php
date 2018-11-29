65121525782f64b50246b0d4e9b8a275c29dfe97
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-11-29 12:49:54 +0200

    fix namespace

--- src/DependencyInjection/VisualCraftDeserializerExtension.php
+++ src/DependencyInjection/VisualCraftDeserializerExtension.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace VisualCraft\DeserializerBundle\DependencyInjection;
+
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
